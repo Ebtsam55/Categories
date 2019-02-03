ce3e761fe911c518d0a7892203a71880537a4949
EbtsamOsama <ebtsamosama25@gmail.com>
2019-02-03 14:54:17 +0200

    ADDED: Recommendations Request

---
+++ app/src/main/java/com/example/reda/categories/RecommendsToModel.java
@@ -0,0 +1,108 @@
+package com.example.reda.categories;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class RecommendsToModel implements Serializable {
+    @SerializedName("created_at")
+    private String created_at;
+
+    @SerializedName("updated_at")
+    private String updated_at;
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("user_id")
+    private int user_id;
+
+    @SerializedName("recommend_id")
+    private int recommend_id;
+    @SerializedName("product_id")
+    private int product_id;
+
+    @SerializedName("message")
+    private String message;
+
+    @SerializedName("recommend")
+    private UserRecommendsToModel recommend_from;
+
+    public RecommendedProduct getRecommend_product() {
+        return recommend_product;
+    }
+
+    public void setRecommend_product(RecommendedProduct recommend_product) {
+        this.recommend_product = recommend_product;
+    }
+
+    @SerializedName("product")
+    private RecommendedProduct recommend_product;
+
+    public RecommendsToModel() {
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRecommend_id() {
+        return recommend_id;
+    }
+
+    public void setRecommend_id(int recommend_id) {
+        this.recommend_id = recommend_id;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UserRecommendsToModel getRecommend_from() {
+        return recommend_from;
+    }
+
+    public void setRecommend_from(UserRecommendsToModel recommend_from) {
+        this.recommend_from = recommend_from;
+    }
+}
